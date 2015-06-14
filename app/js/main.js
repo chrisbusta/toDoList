f22a11c3310209ca161c964f14b6d376e3815aaa
chris bustamante <chrisbusta@hotmsil.com>
2015-06-14 12:51:47 -0400

    final changes

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,69 @@
-console.log('This is cool!');
+(function(){
+  'use strict';
 
-$('.container').html(template.example({
-  value: 'Hello, World!!'
-}));
+var Todo = function(options){
+
+  var args = options || {};
+  this.task = args.task;
+  this.status = 'open';
+
+};
+
+var bin = [];
+
+$('#todoform').on('submit', function(e){
+  e.preventDefault();
+
+  var taskText = $("#new-text").val();
+  var taskInstance = new Todo({task: taskText});
+
+  bin.push(taskInstance);
+  $("#todo").append('<li>' + taskText + '</li>');
+  this.reset();
+});
+
+$('#todo').on('click', 'li', function(e){
+  e.preventDefault();
+  $(this).addClass("complete");
+
+ var tTask = $(this).text();
+
+ var textEdit = _.find(bin, {task: tTask});
+ textEdit.status = 'closed';
+
+});
+
+$('#todo').on('click', '.complete', function(e){
+  e.preventDefault();
+  $(this).removeClass("complete");
+
+ var tTask = $(this).text();
+
+ var textEdit = _.find(bin, {task: tTask});
+ textEdit.status = 'open';
+
+});
+
+$('#remove-button').on('click', function(){
+  bin = bin.filter(function(l){
+   if(l.status === 'open'){
+    return l;
+   }
+   bin.push(l);
+
+
+  });
+  $('#todo').empty();
+  bin.forEach(function(l){
+  $("#todo").append('<li>' + l.task + '</li>');
+  });
+
+});
+
+
+
+
+
+
+
+}());
